1bea3b27414f694ac9711d1e33312539c6dcad7e
hd1801 <hardikdhawan0108@gmail.com>
2021-05-01 23:52:12 +0530

    initial commit

---
+++ Lab2.1.bash
@@ -0,0 +1,18 @@
+#                             Online Bash Shell.
+#                 Code, Compile, Run and Debug Bash script online.
+# Write your code in this editor and press "Run" button to execute it.
+
+
+echo "enter a,b"
+read a
+read b
+area=`expr $a \* $b` 
+echo "area is $area"
+echo "enter sides"
+read s
+area =`expr $s \* $s`
+echo "area is $area" 
+echo "enter r"
+read r
+area=`expr 2 \* $r \*3.14`
+echo "area is $area"
---
+++ Lab2.2.bash
@@ -0,0 +1,10 @@
+#                             Online Bash Shell.
+#                 Code, Compile, Run and Debug Bash script online.
+# Write your code in this editor and press "Run" button to execute it.
+
+
+echo "enter p,n,r"
+read p
+read r
+read n
+echo "simple interest is "$((($p*$r*$n)/100))
