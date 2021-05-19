381eaf8712b108715d9060c8dd440e8b36edde7d
hd1801 <hardikdhawan0108@gmail.com>
2021-05-19 19:26:58 +0530

    lab 5 18-05-2021

---
+++ lab5.1.sh
@@ -0,0 +1,11 @@
+echo "Enter  a no : "
+read n 
+if [ $n -gt 0 ]
+then 
+echo "India is Great"
+elif [ $n -eq 0 ]
+then
+echo "Welcome to India"
+else
+echo "India is not Great"
+fi
---
+++ lab5.2.sh
@@ -0,0 +1,20 @@
+j=1
+while [ $j -lt 20 ]
+do
+i=2
+c=0
+while [ $i -le `expr $j / 2` ]
+do
+r=`expr $j % $i`
+if [ $r -eq 0 ]
+then
+c=`expr $c + 1`
+fi
+i=`expr $i + 1`
+done
+if [ $c -eq 0 ]
+then
+echo $j
+fi
+j=`expr $j + 1`
+done
