33622e161ca7c4095f9ff3a332b3d445f8823c25
hd1801 <hardikdhawan0108@gmail.com>
2021-06-28 14:25:57 +0530

    lab 11

--- lab11.6.sh
+++ lab11.6.sh
@@ -1,4 +1,4 @@
-# to use continue statement for printing number from  1 to 5 and skip 2
+# to use continue statement for printing number from  1 to 5 except 2
 for((i=1;i<=5;i++))
 do
 if [ $i -eq 2 ]
